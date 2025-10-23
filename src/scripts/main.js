b992c90d1930604fec9caf73caad4ee5ca70389e
SheFing <shevchukm2002@gmail.com>
2025-10-24 02:18:09 +0300

    att1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,252 @@
 'use strict';
 
-// write code here
+const body = document.querySelector('body');
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const headers = table.querySelectorAll('thead th');
+const allRows = tbody.querySelectorAll('tr');
+
+const officesLocation = [
+  `Tokyo`,
+  `Singapore`,
+  `London`,
+  `New York`,
+  `Edinburgh`,
+  `San Francisco`,
+];
+
+const notificationTypes = {
+  succes: { title: 'Success!', description: 'User was added!' },
+  error: { title: 'Error!', description: 'Please, fill all inputs!' },
+};
+
+let sortedBy = '';
+let prevActiveRow = '';
+
+// #sorting rows
+headers.forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const rows = [...tbody.querySelectorAll('tr')];
+
+    if (sortedBy === header.textContent) {
+      tbody.innerHTML = '';
+      rows.reverse();
+      rows.forEach((row) => tbody.appendChild(row));
+
+      return;
+    }
+
+    rows.sort((rowA, rowB) => {
+      const cellA = rowA.children[index].textContent.trim();
+      const cellB = rowB.children[index].textContent.trim();
+
+      if (header.textContent === 'Salary') {
+        const numA = parseFloat(cellA.replace(/[^0-9.-]+/g, ''));
+        const numB = parseFloat(cellB.replace(/[^0-9.-]+/g, ''));
+
+        return numA - numB;
+      }
+
+      if (header.textContent === 'Age') {
+        return Number(cellA) - Number(cellB);
+      }
+
+      return cellA.localeCompare(cellB);
+    });
+
+    tbody.innerHTML = '';
+    rows.forEach((row) => tbody.appendChild(row));
+    sortedBy = header.textContent;
+  });
+});
+
+// #selecting row
+allRows.forEach((selectedRow) => {
+  selectedRow.addEventListener('click', (clickedRow) => {
+    if (prevActiveRow) {
+      prevActiveRow.removeAttribute('class');
+    }
+
+    if (selectedRow === prevActiveRow) {
+      selectedRow.removeAttribute('class');
+
+      return;
+    }
+
+    selectedRow.setAttribute('class', 'active');
+    prevActiveRow = selectedRow;
+  });
+});
+
+// #creating form
+function createForm() {
+  function createInput(nameIn, typeInput = 'input') {
+    function formatLabel(key) {
+      return key.charAt(0).toUpperCase() + key.slice(1) + ':';
+    }
+
+    function addAttributesForInput(input) {
+      input.setAttribute('name', nameIn);
+      input.setAttribute('type', 'text');
+      input.setAttribute('data-qa', nameIn);
+      input.required = true;
+
+      if (nameIn === 'salary' || nameIn === 'age') {
+        input.type = 'number';
+      }
+    }
+
+    const label = document.createElement('label');
+
+    label.textContent = formatLabel(nameIn);
+
+    if (typeInput === 'input') {
+      const input = document.createElement('input');
+
+      addAttributesForInput(input);
+      label.append(input);
+    }
+
+    if (typeInput === 'select') {
+      const select = document.createElement('select');
+
+      addAttributesForInput(select);
+
+      officesLocation.forEach((item, index) => {
+        const option = document.createElement('option');
+
+        option.setAttribute('value', index);
+        option.textContent = item;
+
+        select.append(option);
+      });
+      label.append(select);
+    }
+
+    return label;
+  }
+
+  const form = document.createElement('form');
+
+  form.setAttribute('class', 'new-employee-form');
+
+  const nameLabel = createInput('name');
+  const positionLabel = createInput('position');
+  const ageLabel = createInput('age');
+  const salaryLabel = createInput('salary');
+
+  const officeInput = createInput('office', 'select');
+
+  const submitBtn = document.createElement('button');
+
+  submitBtn.textContent = 'Save to table';
+
+  form.append(
+    nameLabel,
+    positionLabel,
+    officeInput,
+    ageLabel,
+    salaryLabel,
+    submitBtn,
+  );
+
+  document.querySelector('body').append(form);
+}
+
+function clearAllInputs() {
+  const form = document.querySelector('form');
+  const allInputs = form.querySelectorAll('input, select');
+
+  allInputs.forEach((input) => {
+    if (input.tagName === 'SELECT') {
+      input.selectedIndex = 0;
+    } else {
+      input.value = '';
+    }
+  });
+}
+
+function createUser(user) {
+  const newUser = document.createElement('tr');
+
+  for (const userProp in user) {
+    const newCell = document.createElement('td');
+
+    if (userProp === 'userSalary') {
+      newCell.textContent = `$${Number(user[userProp]).toLocaleString()}`;
+    } else {
+      newCell.textContent = user[userProp];
+    }
+
+    newUser.append(newCell);
+  }
+
+  tbody.append(newUser);
+}
+
+function createNotification(type, isSuccess) {
+  const notify = document.createElement('notification');
+
+  notify.setAttribute('data-qa', 'notification');
+
+  notify.innerHTML = `
+    <h2 class="title">${type.title}</h2>
+    <p class="text">${type.description}</p>
+  `;
+
+  notify.className = 'notification';
+
+  // eslint-disable-next-line no-unused-expressions
+  isSuccess
+    ? (notify.className = 'notification success')
+    : (notify.className = 'notification error');
+
+  body.append(notify);
+
+  setTimeout(() => {
+    notify.remove();
+  }, 2000);
+}
+
+createForm();
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (eventBtn) => {
+  eventBtn.preventDefault();
+
+  const newUser = {};
+
+  newUser.userName = document.querySelector('[name="name"]').value;
+  newUser.userPosition = document.querySelector('[name="position"]').value;
+
+  newUser.userOffice =
+    officesLocation[+document.querySelector('[name="office"]').value];
+  newUser.userAge = document.querySelector('[name="age"]').value;
+  newUser.userSalary = document.querySelector('[name="salary"]').value;
+
+  let isAllInputsFilled = true;
+
+  for (const userProp in newUser) {
+    if (!newUser[userProp]) {
+      isAllInputsFilled = false;
+      break;
+    }
+  }
+
+  if (
+    newUser.userAge < 18 ||
+    newUser.userAge > 90 ||
+    newUser.userName.length < 4
+  ) {
+    isAllInputsFilled = false;
+  }
+
+  if (isAllInputsFilled) {
+    clearAllInputs();
+    createUser(newUser);
+    createNotification(notificationTypes.succes, true);
+  } else {
+    createNotification(notificationTypes.error, false);
+  }
+});
